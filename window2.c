ca13a4450b51a05cadddeb1a7d1f42b45fcc2583
abakhcha <abakhcha@e1r3p8.1337.ma>
2024-03-15 15:31:53 +0000

    norm

--- window2.c
+++ window2.c
@@ -6,7 +6,7 @@
 /*   By: abakhcha <abakhcha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 15:08:39 by abakhcha          #+#    #+#             */
-/*   Updated: 2024/03/15 15:13:58 by abakhcha         ###   ########.fr       */
+/*   Updated: 2024/03/15 15:31:30 by abakhcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	ft_loadhouse_image(void *mlx, void *win, int *l, int *j)
 	house.relative_path = "img/house.xpm";
 	house.img = mlx_xpm_file_to_image(mlx, house.relative_path,
 			&house.img_width, &house.img_height);
-	if(!house.img || !house.relative_path)
+	if (!house.img || !house.relative_path)
 		error_print("image problem !!\n");
 	mlx_put_image_to_window(mlx, win, house.img, *l, *j);
 }
@@ -31,7 +31,7 @@ void	ft_lodwalls_image(void *mlx, void *win, int *l, int *j)
 	walls.relative_path = "img/walls.xpm";
 	walls.img = mlx_xpm_file_to_image(mlx, walls.relative_path,
 			&walls.img_width, &walls.img_height);
-	if(!walls.img || !walls.relative_path)
+	if (!walls.img || !walls.relative_path)
 		error_print("image problem !!\n");
 	mlx_put_image_to_window(mlx, win, walls.img, *l, *j);
 }
@@ -43,7 +43,7 @@ void	ft_lodcollect_image(void *mlx, void *win, int *l, int *j)
 	collect.relative_path = "img/collect.xpm";
 	collect.img = mlx_xpm_file_to_image(mlx, collect.relative_path,
 			&collect.img_width, &collect.img_height);
-	if(!collect.img || !collect.relative_path)
+	if (!collect.img || !collect.relative_path)
 		error_print("image problem !!\n");
 	mlx_put_image_to_window(mlx, win, collect.img, *l, *j);
 }
@@ -55,7 +55,7 @@ void	ft_lodlard_image(t_mlx *mlx, int *l, int *j)
 	lard.relative_path = "img/lard.xpm";
 	lard.img = mlx_xpm_file_to_image(mlx->mlx, lard.relative_path,
 			&lard.img_width, &lard.img_height);
-	if(!lard.img || !lard.relative_path)
+	if (!lard.img || !lard.relative_path)
 		error_print("image problem !!\n");
 	mlx_put_image_to_window(mlx->mlx, mlx->win, lard.img, *l, *j);
 }
